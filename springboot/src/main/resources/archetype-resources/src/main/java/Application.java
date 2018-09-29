a2584ef7c86a2f65c65247753d6400016a92d63c
AiJiangnan <904629998@qq.com>
2018-09-29 10:38:03 +0800

    git init

---
+++ springboot/src/main/resources/archetype-resources/src/main/java/Application.java
@@ -0,0 +1,17 @@
+package ${package};
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+@EnableBatchProcessing
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringbootApplication.class, args);
+    }
+
+}
